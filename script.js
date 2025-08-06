87d06b2952df20d0cf956676fb9a24fc8bab2029
Karl-codeing <karlmagzucker@gmail.com>
2025-08-06 22:15:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 import { auth } from './firebase-config.js';
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-// Login
-const loginBtn = document.getElementById('loginBtn');
-if (loginBtn) {
-  loginBtn.addEventListener('click', () => {
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => window.location.href = 'shop.html')
-      .catch(err => alert(err.message));
-  });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const loginBtn = document.getElementById('loginBtn');
+  if (loginBtn) {
+    loginBtn.onclick = () => {
+      const email = document.getElementById('loginEmail').value;
+      const pwd = document.getElementById('loginPassword').value;
+      signInWithEmailAndPassword(auth, email, pwd)
+        .then(() => window.location.href = 'shop.html')
+        .catch(err => alert(err.message));
+    };
+  }
 
-// Register
-const registerBtn = document.getElementById('registerBtn');
-if (registerBtn) {
-  registerBtn.addEventListener('click', () => {
-    const email = document.getElementById('registerEmail').value;
-    const password = document.getElementById('registerPassword').value;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(() => window.location.href = 'index.html')
-      .catch(err => alert(err.message));
-  });
-}
+  const regBtn = document.getElementById('registerBtn');
+  if (regBtn) {
+    regBtn.onclick = () => {
+      const email = document.getElementById('registerEmail').value;
+      const pwd = document.getElementById('registerPassword').value;
+      createUserWithEmailAndPassword(auth, email, pwd)
+        .then(() => window.location.href = 'index.html')
+        .catch(err => alert(err.message));
+    };
+  }
+});
